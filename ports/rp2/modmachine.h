d4a7b5ec25aedd469c493e6024ebb3203bd608d3
jd <jd@werther.fritz.box>
2022-04-13 23:00:03 +0200

    start implementing DMX (Digital MultiPleX) node - add new module "machine.uartdmx" based on machine.uart - delete methods not needed for DMX decoder

--- ports/rp2/modmachine.h
+++ ports/rp2/modmachine.h
@@ -11,6 +11,7 @@ extern const mp_obj_type_t machine_rtc_type;
 extern const mp_obj_type_t machine_spi_type;
 extern const mp_obj_type_t machine_timer_type;
 extern const mp_obj_type_t machine_uart_type;
+extern const mp_obj_type_t machine_uart_dmx_type;
 extern const mp_obj_type_t machine_wdt_type;
 
 void machine_pin_init(void);
